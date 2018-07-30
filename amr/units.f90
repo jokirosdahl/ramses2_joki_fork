fc27801e043f68deba214d2b7ee24ed924f8ce07
Romain Teyssier <romain.teyssier@gmail.com>
2018-07-30 13:42:48 +0200

    Remove patches. No more patches. Only DEFINE.

--- amr/units.f90
+++ amr/units.f90
@@ -1,6 +1,6 @@
 subroutine units(r,g,scale_l,scale_t,scale_d,scale_v,scale_nH,scale_T2)
   use amr_parameters, only: dp,ndim,kB,mH,X_H,rhoc
-  use amr_commons, only: run_t,global_t,mesh_t
+  use amr_commons, only: run_t,global_t
   implicit none
   type(run_t)::r
   type(global_t)::g
@@ -10,25 +10,32 @@ subroutine units(r,g,scale_l,scale_t,scale_d,scale_v,scale_nH,scale_T2)
   ! For gravity runs, make sure that G=1 in user units.
   !-----------------------------------------------------------------------
 
-  ! scale_d converts mass density from user units into g/cc
-  scale_d = r%units_density
-  if(r%cosmo) scale_d = g%omega_m * rhoc *(g%h0/100.)**2 / g%aexp**3
+  ! Default units from namelist
+  scale_d = r%units_density      ! scale_d converts mass density from user units into g/cc
+  scale_t = r%units_time         ! scale_t converts time from user units into seconds
+  scale_l = r%units_length       ! scale_l converts distance from user units into cm
+  scale_v = scale_l / scale_t    ! scale_v converts velocity in user units into cm/s
+  scale_T2 = mH/kB * scale_v**2  ! scale_T2 converts (P/rho) in user unit into (T/mu) in Kelvin
+  scale_nH = X_H/mH * scale_d    ! scale_nH converts rho in user units into nH in H/cc
 
-  ! scale_t converts time from user units into seconds
-  scale_t = r%units_time
-  if(r%cosmo) scale_t = g%aexp**2 / (g%h0*1d5/3.08d24)
+  ! Units for cosmological simulations are hard-coded
+  if(r%cosmo)then
+     scale_d = g%omega_m * rhoc *(g%h0/100.)**2 / g%aexp**3
+     scale_t = g%aexp**2 / (g%h0*1d5/3.08d24)
+     scale_l = g%aexp * g%boxlen_ini * 3.08d24 / (g%h0/100)
+     scale_v = scale_l / scale_t    ! scale_v converts velocity in user units into cm/s
+     scale_T2 = mH/kB * scale_v**2  ! scale_T2 converts (P/rho) in user unit into (T/mu) in Kelvin
+     scale_nH = X_H/mH * scale_d    ! scale_nH converts rho in user units into nH in H/cc
+  endif
 
-  ! scale_l converts distance from user units into cm
-  scale_l = r%units_length
-  if(r%cosmo) scale_l = g%aexp * g%boxlen_ini * 3.08d24 / (g%h0/100)
+#ifdef COEUR
+  scale_d = 0.443201646421875d-17  ! scale_d converts mass density from user units into g/cc
+  scale_t = 0.183887441975074d+13  !1/(G*d0)**0.5
+  scale_l = 25*0.478712000000000d+18  !25*32000 AU
+  scale_v = scale_l / scale_t    ! scale_v converts velocity in user units into cm/s
+  scale_T2 = mH/kB * scale_v**2  ! scale_T2 converts (P/rho) in user unit into (T/mu) in Kelvin
+  scale_nH = 1.0/mH * scale_d    ! scale_nH converts rho in user units into nH in H/cc
+#endif
 
-  ! scale_v converts velocity in user units into cm/s
-  scale_v = scale_l / scale_t
-
-  ! scale_T2 converts (P/rho) in user unit into (T/mu) in Kelvin
-  scale_T2 = mH/kB * scale_v**2
-
-  ! scale_nH converts rho in user units into nH in H/cc
-  scale_nH = X_H/mH * scale_d
 
 end subroutine units
--- hydro/condinit.f90
+++ hydro/condinit.f90
@@ -33,11 +33,80 @@ subroutine condinit(r,g,x,u,dx,nn)
 #endif
   real(dp),dimension(1:nvector,1:nvar)::q   ! Primitive variables
 
+#ifdef COEUR
+  integer::i
+  real(dp)::r2,rx,ry,rz,d,p,vx,vy,vz,r_trunc,r2_trunc,c2
+  real(dp)::omega_code,AU,Msol,pi,M,sigma,r_min,r2_min,omega_const,r_vortex,invr2_vortex
+  real(dp)::scale_nH,scale_T2,scale_l,scale_d,scale_t,scale_v,scale_m
+#endif
+
   ! Call built-in initial condition generator
   call region_condinit(r,g,x,q,dx,nn)
 
   ! Add here, if you wish, some user-defined initial conditions
   ! ........
+
+#ifdef COEUR
+
+  call units(r,g,scale_l,scale_t,scale_d,scale_v,scale_nH,scale_T2)
+  scale_m=scale_d*scale_l**3
+
+  ! constants
+  AU=1.49598d13
+  Msol= 1.98892d33
+  pi=3.14159
+
+  ! mass, radius, and ratio of rotational to gravitational energy
+  r_trunc=25*4000.*AU/scale_l
+  r_min=10.*AU/scale_l
+  r_vortex=4000.*AU/scale_l
+  M=100.*Msol/scale_m
+
+  sigma=M/(4*pi*r_trunc)!*1.00015!*1.016
+
+  r2_trunc=r_trunc**2
+  r2_min=r_min**2
+  invr2_vortex=1./r_vortex**2
+
+  omega_const=0.1*sqrt(1./r2_trunc+invr2_vortex)*sqrt(M/r_trunc)
+
+  c2=(18939.2/(scale_l/scale_t))**2
+
+  do i=1,nn
+     rx=x(i,1)-r%boxlen/2.
+     ry=x(i,2)-r%boxlen/2.
+     rz=x(i,3)-r%boxlen/2.
+
+     !density/pressure
+     r2=rx**2+ry**2+rz**2
+     d=sigma/(r2+r2_min)
+     omega_code=omega_const/sqrt(1.+invr2_vortex*r2)
+     if (r2>=r2_trunc)then
+        d=d*1.e-4
+        omega_code=omega_code/sqrt(r2)*exp(10.*(r2_trunc-r2))
+     end if
+
+     !pressure
+     p=d*c2
+
+     !velocity
+!     vx=-1./(3.**0.5)*omega_code*(ry-rz)
+!     vy=-1./(3.**0.5)*omega_code*(rz-rx)
+!     vz=-1./(3.**0.5)*omega_code*(rx-ry)
+
+     !velocity
+     vx=-1.*omega_code*ry
+     vy=omega_code*rx
+     vz=0.
+
+     q(i,1)=d
+     q(i,2)=vx
+     q(i,3)=vy
+     q(i,4)=vz
+     q(i,5)=p
+  end do
+
+#endif
 
   ! Convert primitive to conservative variables
   ! density -> density
