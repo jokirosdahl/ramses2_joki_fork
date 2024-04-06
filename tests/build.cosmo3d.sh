dab6f94300b63c06dd50ad8b7ab7e7563eb4ef2f
Romain Teyssier <romain.teyssier@gmail.com>
2024-04-06 13:01:13 -0400

    Update test scripts and remove old namelists.

--- tests/build.cosmo3d.sh
+++ tests/build.cosmo3d.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd bin
-make NDIM=3 MPI=1 UNITS=cosmo CONDINIT=default HYDRO=1 GRAV=1 NVAR=7
+make NDIM=3 MPI=1 UNITS=cosmo HYDRO=1 GRAV=1 NVAR=7
 
--- tests/build.hydro2d.sh
+++ tests/build.hydro2d.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd bin
-make NDIM=2 MPI=1  UNITS=default CONDINIT=default GRAV=0 HYDRO=1
+make NDIM=2 MPI=1 HYDRO=1
 
