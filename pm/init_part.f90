19225a46443d39564f77c49ab7e818a5bdb37a98
Romain Teyssier <romain.teyssier@gmail.com>
2024-04-19 16:01:43 -0400

    Add halo patch vector potential.

--- pm/init_part.f90
+++ pm/init_part.f90
@@ -35,7 +35,7 @@ end subroutine r_init_part
 subroutine init_part(r,g,p)
   use amr_parameters, only: ndim
   use amr_commons, only: run_t,global_t
-  use pm_parameter, only: DM_TYPE
+  use pm_parameters, only: DM_TYPE
   use pm_commons, only: part_t
   implicit none
   type(run_t)::r
@@ -72,7 +72,7 @@ end subroutine init_part
 subroutine init_star(r,g,s)
   use amr_parameters, only: ndim
   use amr_commons, only: run_t,global_t
-  use pm_parameter, only: STAR_TYPE
+  use pm_parameters, only: STAR_TYPE
   use pm_commons, only: part_t
   implicit none
   type(run_t)::r
