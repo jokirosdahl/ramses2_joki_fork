b4553a89e9faccf2a2511ab2a90ca895bc8c2e9e
Romain Teyssier <romain.teyssier@gmail.com>
2024-04-26 19:29:19 -0400

    NENER tested in 1D.

--- hydro/write_screen.f90
+++ hydro/write_screen.f90
@@ -1,6 +1,7 @@
 subroutine write_screen(r,m)
   use amr_parameters, only: dp,ndim
   use amr_commons, only: mesh_t, run_t
+  use hydro_parameters, only: nener
   implicit none
   type(run_t)::r
   type(mesh_t)::m
@@ -12,9 +13,13 @@ subroutine write_screen(r,m)
 #ifdef MHD
   real(kind=8),dimension(:),allocatable::AA,BB,CC
 #endif
+#if NENER>0
+  integer::irad
+  real(kind=8),dimension(:,:),allocatable::prad
+#endif
   integer::igrid,ind
   logical::leaf
-  real(kind=8)::ekin,emag
+  real(kind=8)::ekin,emag,erad
   
 #ifdef HYDRO
 
@@ -40,6 +45,9 @@ subroutine write_screen(r,m)
      allocate(uu(1:nleaf))
      allocate(vv(1:nleaf))
      allocate(ww(1:nleaf))
+#if NENER>0
+     allocate(prad(1:nener,1:nleaf))
+#endif
 #ifdef MHD
      allocate(AA(1:nleaf))
      allocate(BB(1:nleaf))
@@ -70,7 +78,14 @@ subroutine write_screen(r,m)
                     CC(nleaf)=0.5*(m%grid(igrid)%bold(ind,3)+m%grid(igrid)%bold(ind,6))
                     emag=0.5*(AA(nleaf)**2+BB(nleaf)**2+CC(nleaf)**2)
 #endif
-                    pp(nleaf)=m%grid(igrid)%uold(ind,5)-ekin-emag
+                    erad=0.0d0
+#if NENER>0
+                    do irad=1,nener
+                       erad=erad+m%grid(igrid)%uold(ind,5+irad)
+                       prad(irad,nleaf)=(r%gamma_rad(irad)-1)*m%grid(igrid)%uold(ind,5+irad)
+                    end do
+#endif
+                    pp(nleaf)=m%grid(igrid)%uold(ind,5)-ekin-emag-erad
                     pp(nleaf)=(r%gamma-1)*pp(nleaf)
 
                     if(ABS(uu(nleaf))<1d-99)uu(nleaf)=0.0D0
@@ -106,6 +121,20 @@ subroutine write_screen(r,m)
              & CC(ii(i))
      end do
      write(*,*)'================================================'
+#elif NENER>0
+     write(*,*)'================================================'
+     write(*,114)nleaf
+     write(*,*)'lev      x           d          u          P        P_rad'
+     do i=1,nleaf
+        write(*,116)      &
+             & ll(ii(i)), &
+             & xx(i),     &
+             & dd(ii(i)), &
+             & uu(ii(i)), &
+             & pp(ii(i)), &
+             & prad(1,ii(i))
+     end do
+     write(*,*)'================================================'
 #else
      write(*,*)'================================================'
      write(*,114)nleaf
@@ -128,6 +157,9 @@ subroutine write_screen(r,m)
      deallocate(ww)
      deallocate(pp)
      deallocate(ii)
+#if NENER>0
+     deallocate(prad)
+#endif
 #ifdef MHD
      deallocate(AA)
      deallocate(BB)
@@ -139,6 +171,7 @@ subroutine write_screen(r,m)
 113 format(i3,1x,1pe12.5,1x,3(1pe10.3,1x))
 114 format(' Output ',i5,' cells')
 115 format(i3,1x,1pe12.5,1x,8(1pe10.3,1x))
+116 format(i3,1x,1pe12.5,1x,4(1pe10.3,1x))
 
 #endif
 
