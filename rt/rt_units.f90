2e6c3d6d4197d4a68f3aa17cdefea072dc5e8e2e
Romain Teyssier <romain.teyssier@gmail.com>
2025-01-25 11:45:25 -0500

    Remove some unnecessary constants.

--- rt/rt_units.f90
+++ rt/rt_units.f90
@@ -1,21 +1,20 @@
 subroutine rt_units(r,g,scale_Np,scale_Fp)
   use amr_parameters, only: dp,ndim
-  use constants, only: kB,mH,X_H,rhoc
   use amr_commons, only: run_t,global_t
   implicit none
   type(run_t)::r
   type(global_t)::g
   real(kind=8)::scale_Np, scale_Fp
-  real(kind=8)::scale_nH,scale_T2,scale_t,scale_v,scale_d,scale_l
+  real(kind=8)::scale_nH, scale_T2, scale_t, scale_v, scale_d, scale_l
   !-----------------------------------------------------------------------
   ! Conversion factors from user units into cgs units for RT variables.
-  ! scale_Np   <=  photon number density scale [# cm-3]
-  ! scale_Fp   <=  photon flux density scale [# cm-2 s-1]
+  ! scale_Np <= photon number density scale [# cm-3]
+  ! scale_Fp <= photon flux density scale [# cm-2 s-1]
   !-----------------------------------------------------------------------
 
 #define COSMO 1
 
-  call units(r, g, scale_l,scale_t,scale_d,scale_v,scale_nH,scale_T2)
+  call units(r, g, scale_l, scale_t, scale_d, scale_v, scale_nH, scale_T2)
 
 #if UNITS==COSMO
   ! Units for cosmological simulations are hard-coded
