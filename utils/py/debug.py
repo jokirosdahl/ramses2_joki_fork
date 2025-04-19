2de906922cc31d9aaae7bd3892f71d755d6f9a90
Romain Teyssier <romain.teyssier@gmail.com>
2025-04-19 15:54:06 -0400

    Add depug.py routine.

--- utils/py/debug.py
+++ utils/py/debug.py
@@ -8,13 +8,8 @@ import miniramses as ram
 parser = argparse.ArgumentParser()
 parser.add_argument("nout", help="enter output number")
 parser.add_argument("--path", help="specify a path")
-parser.add_argument("--log", help="plot log variable",action="store_true")
 parser.add_argument("--bkp", help="plot log variable",action="store_true")
-parser.add_argument("--out", help="output a png image")
 parser.add_argument("--prefix", help="specify a file prefix")
-parser.add_argument("--min", help="specify a minimum variable value")
-parser.add_argument("--max", help="specify a maximum variable value")
-parser.add_argument("--var", help="specify a variable number")
 parser.add_argument("--xcen", help="specify the image center x-coordinate")
 parser.add_argument("--ycen", help="specify the image center y-coordinate")
 parser.add_argument("--zcen", help="specify the image center z-coordinate")
@@ -23,14 +18,10 @@ args = parser.parse_args()
 # path the the file
 path = args.path
 prefix = args.prefix
-ivar = args.var
-vmin = args.min
-vmax = args.max
 radius = args.rad
 xcenter = args.xcen
 ycenter = args.ycen
 zcenter = args.zcen
-log = args.log
 backup = args.bkp
 
 if xcenter==None:
@@ -50,48 +41,45 @@ if radius==None:
 else:
     radius=float(radius)
 center=np.array([xcenter,ycenter,zcenter])
-log0=None
-if log:
-    log0=1
-if ivar==None:
-    ivar=0
-else:
-    ivar=int(ivar)-1
-if prefix==None:
-    prefix="hydro"
 if path==None:
     path="./"
 else:
     path=path+"/"
-if prefix=="hydro":
-    isort=0
-if prefix=="peak":
-    isort=1
-if prefix=="grav":
-    isort=0
-if prefix=="rt":
-    isort=0
 
 nout = args.nout
 print("Reading output number ",nout)
 
-c=ram.rd_cell(nout,path=path,prefix=prefix,center=center,radius=radius,backup=backup)
-i=ram.rd_info(nout,path=path,backup=backup)
-m_p=1.66e-24
-k_b=1.38e-16
+i=ram.rd_info(nout,backup=backup)
+c=ram.rd_cell(nout,backup=backup)
+
 d=c.u[0]
 if(backup):
-    p=(i.gamma-1)*(c.u[4]-0.5*(c.u[1]**2+c.u[2]**2+c.u[3]**2)/c.u[0])
+    ux=c.u[1]/c.u[0]
+    uy=c.u[2]/c.u[0]
+    uz=c.u[3]/c.u[0]
+    bx=0.5*(c.u[7]+c.u[10])
+    by=0.5*(c.u[8]+c.u[11])
+    bz=0.5*(c.u[9]+c.u[12])
+    emag=0.5*(bx*bx+by*by+bz*bz)
+else:
+    ux=c.u[1]
+    uy=c.u[2]
+    uz=c.u[3]
+if(backup):
+    ekin=0.5*d*(ux*ux+uy*uy+uz*uz)
+    p=2/3*(c.u[4]-ekin-emag)
 else:
     p=c.u[4]
+cs=np.sqrt(5/3*p/d)
+dt=c.dx/(3*cs+np.abs(ux)+np.abs(uy)+np.abs(uz))
 
-plt.hist2d(np.log10(d*i.unit_d/m_p),np.log10(p/d/k_b*m_p*(i.unit_l/i.unit_t)**2),weights=c.u[0]*c.dx**3,density=True,bins=100,norm=colors.LogNorm())
-plt.xlabel('log10(n_H) [H/cc]')
-plt.ylabel('log10(T/mu) [K]')
-plt.title('Mass-weighted Histogram with Logarithmic Color Scale')
+print("min d=",np.min(d)," max d=",np.max(d))
+print("max ux=",np.max(np.abs(ux)))
+print("max uy=",np.max(np.abs(uy)))
+print("max uz=",np.max(np.abs(uz)))
+print("max cs=",np.max(np.abs(cs)))
+print("min dt=",np.min(np.abs(dt)))
 
-if args.out:
-    plt.savefig(args.out)
 
-plt.show()
+
 
