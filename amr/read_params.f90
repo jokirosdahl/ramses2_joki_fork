471454be4c43738b97745f83e28c74d46bca0517
Eric Moseley <ericm628@gmail.com>
2025-08-19 16:01:18 -0700

    .

--- amr/read_params.f90
+++ amr/read_params.f90
@@ -1229,30 +1229,33 @@ subroutine m_read_params(pst)
 #endif
 
   !--------------------------------------------------
-  ! Validate Riemann solver exists
+  ! Validate Riemann solver exists and was properly assigned
   !--------------------------------------------------
 #ifdef HYDRO
   if(hydro) then
 #ifndef MHD
-     if(s%r%riemann==0) then
-        write(*,*) 'Error in namelist: unknown riemann="'//TRIM(riemann)//'"'
-        write(*,*) 'Allowed (HYDRO): llf, hll, hllc'
+     if(riemann/='llf' .and. riemann/='hll' .and. riemann/='hllc') then
+        write(*,*) 'Error: riemann="'//TRIM(riemann)//'" was not properly assigned to a valid solver'
+        write(*,*) 'This may indicate a compilation issue or missing solver implementation'
         call mdl_abort(s%mdl)
      endif
 #else
-     if(s%r%riemann==0) then
+     if(riemann/='llf' .and. riemann/='hll' .and. riemann/='hlld' .and. riemann/='roe' .and. riemann/='upwind') then
         write(*,*) 'Error in namelist: unknown riemann="'//TRIM(riemann)//'"'
         write(*,*) 'Allowed (MHD): llf, hll, hlld, roe, upwind'
         call mdl_abort(s%mdl)
      endif
-     if(s%r%riemann2d==0) then
+     if(riemann2d/='none' .and. riemann2d/='llf' .and. riemann2d/='hll' .and. riemann2d/='hllf' &
+      .and. riemann2d/='hlla' .and. riemann2d/='hlld' .and. riemann2d/='roe' .and. riemann2d/='upwind') then
         write(*,*) 'Error in namelist: unknown riemann2d="'//TRIM(riemann2d)//'"'
-        write(*,*) 'Allowed (MHD 2D): llf, hll, hllf, hlla, hlld, roe, upwind; or set riemann2d="none" to reuse riemann'
+        write(*,*) 'Allowed : llf, hll, hllf, hlla, hlld, roe, upwind; or set riemann2d="none" to reuse riemann'
         call mdl_abort(s%mdl)
      endif
 #endif
   endif
 #endif
+
+
   s%r%switch_llf_dmin=switch_llf_dmin
   s%r%switch_llf_pmin=switch_llf_pmin
 
