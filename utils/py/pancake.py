203e8207a052fa3781f31e6e38954bbc226b4e2d
Romain Teyssier <romain.teyssier@gmail.com>
2025-10-01 09:21:03 -0400

    Scipy just retired romberg integration. Replaced by quad.

--- utils/py/pancake.py
+++ utils/py/pancake.py
@@ -1,6 +1,5 @@
 import numpy as np
 import scipy as sp
-import matplotlib.pyplot as plt
 
 """
 Parameters for the pancake collapse test
@@ -24,15 +23,18 @@ def d1(a):
     """
     y = omega_m * (1.0 / a - 1.0) + omega_l * (a**2 - 1.0) + 1.0
     y12 = np.sqrt(y)
-    return y12 / a * sp.integrate.romberg(fy,1e-6,a)
+    integral, error = sp.integrate.quad(fy,1e-6,a)
+    result = y12 / a * integral
+    return result
 
 def fpeebl(a):
     """
     Computes the Pebbles growth factor f = d log D1 / d log a.
     """
     y = omega_m * (1.0 / a - 1.0) + omega_l * (a**2 - 1.0) + 1.0
-    fact = sp.integrate.romberg(fy,1e-6,a)
-    return (omega_l * a**2 - 0.5 * omega_m / a) / y - 1.0 + a * fy(a) / fact
+    fact, error = sp.integrate.quad(fy,1e-6,a)
+    result = (omega_l * a**2 - 0.5 * omega_m / a) / y - 1.0 + a * fy(a) / fact
+    return result
 
 xf = np.linspace(0, 1, n+1)
 xp = 0.5 * ( xf[:-1] + xf[1:] )
