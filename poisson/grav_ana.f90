5d6e491486bf019cc51d4a3fcbc39436df62a377
Romain Teyssier <romain.teyssier@gmail.com>
2025-10-07 19:43:38 -0400

    Start implementing isolated boundary conditions for gravity. Fix a bug in rd_cell.

--- poisson/grav_ana.f90
+++ poisson/grav_ana.f90
@@ -2,12 +2,13 @@
 !#########################################################
 !#########################################################
 !#########################################################
-subroutine grav_ana(x,f,dx,ncell,gravity_type,gravity_params)
+subroutine gravana(r,g,x,f,dx,ncell)
   use amr_parameters, only: ndim, nvector
+  use amr_commons, only: run_t, global_t
   implicit none
-  integer::gravity_type
-  real(kind=8),dimension(1:10)::gravity_params
-  integer ::ncell                         ! Size of input arrays
+  type(run_t)::r
+  type(global_t)::g
+  integer::ncell                              ! Size of input arrays
   real(kind=8)::dx                            ! Cell size
   real(kind=8),dimension(1:nvector,1:ndim)::f ! Gravitational acceleration
   real(kind=8),dimension(1:nvector,1:ndim)::x ! Cell center position.
@@ -20,21 +21,21 @@ subroutine grav_ana(x,f,dx,ncell,gravity_type,gravity_params)
   real(kind=8)::gmass,emass,xmass,ymass,zmass,rr,rx,ry,rz
 
   ! Constant vector
-  if(gravity_type==1)then 
+  if(r%gravity_type==1)then
      do idim=1,ndim
         do i=1,ncell
-           f(i,idim)=gravity_params(idim)
+           f(i,idim)=r%gravity_params(idim)
         end do
      end do
   end if
 
   ! Point mass
-  if(gravity_type==2)then 
-     gmass=gravity_params(1) ! GM
-     emass=gravity_params(2) ! Softening length
-     xmass=gravity_params(3) ! Point mass coordinates
-     ymass=gravity_params(4)
-     zmass=gravity_params(5)
+  if(r%gravity_type==2)then
+     gmass=r%gravity_params(1) ! GM
+     emass=r%gravity_params(2) ! Softening length
+     xmass=r%gravity_params(3) ! Point mass coordinates
+     ymass=r%gravity_params(4)
+     zmass=r%gravity_params(5)
      do i=1,ncell
         rx=0.0d0; ry=0.0d0; rz=0.0d0
         rx=x(i,1)-xmass
@@ -55,39 +56,53 @@ subroutine grav_ana(x,f,dx,ncell,gravity_type,gravity_params)
      end do
   end if
 
-end subroutine grav_ana
+end subroutine gravana
 !#########################################################
 !#########################################################
 !#########################################################
 !#########################################################
-subroutine phi_ana(rr,pp,ngrid,multipole)
-  use amr_commons, only: ndim, nvector
+subroutine phiana(r,g,x,phi,dx,ncell)
+  use amr_parameters, only: ndim, nvector
+  use amr_commons, only: run_t, global_t
   implicit none
-  integer::ngrid
-  real(kind=8),dimension(1:ndim+1)::multipole
-  real(kind=8),dimension(1:nvector)::rr,pp
-  ! -------------------------------------------------------------------
-  ! This routine set up boundary conditions for fine levels.
-  ! -------------------------------------------------------------------
-
+  type(run_t)::r
+  type(global_t)::g
+  integer::ncell                              ! Size of input arrays
+  real(kind=8)::dx                            ! Cell size
+  real(kind=8),dimension(1:nvector)::phi      ! Gravitational potential
+  real(kind=8),dimension(1:nvector,1:ndim)::x ! Cell center position.
+  !================================================================
+  ! This routine computes the potential using analytical models.
+  ! x(i,1:ndim) are cell center position in [0,boxlen] (user units).
+  ! phi(i is the gravitational potential in user units.
+  !================================================================
   integer :: i
-  real(kind=8):: fourpi
+  real(kind=8)::fourpi,rx,ry,rz,rr
 
   fourpi=4.D0*ACOS(-1.0D0)
 
+  do i=1,ncell
+     rx=0.0d0; ry=0.0d0; rz=0.0d0
+     rx=x(i,1)
+#if NDIM>1
+     ry=x(i,2)
+#endif
+#if NDIM>2
+     rz=x(i,3)
+#endif
+     rr=sqrt(rx**2+ry**2+rz**2)
 #if NDIM==1
-  do i=1,ngrid
-     pp(i)=multipole(1)*fourpi/2d0*rr(i)
-  end do
+     phi(i)=g%multipole%q(1)*fourpi/2d0*rr
 #endif
 #if NDIM==2
-  do i=1,ngrid
-     pp(i)=multipole(1)*2d0*log(rr(i))
-  end do
+     phi(i)=g%multipole%q(1)*2d0*log(rr)
 #endif
 #if NDIM==3
-  do i=1,ngrid
-     pp(i)=-multipole(1)/rr(i)
-  end do
+     phi(i)=-g%multipole%q(1)/rr
 #endif
-end subroutine phi_ana
+  end do
+end subroutine phiana
+!#########################################################
+!#########################################################
+!#########################################################
+!#########################################################
