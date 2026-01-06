ea8f98c6725366ce604bdfc1b9b483f32c25251c
Eric Moseley <ericm628@gmail.com>
2026-01-06 15:32:52 -0800

    fixing merge conflict with task manager

--- amr/task_manager.f90
+++ amr/task_manager.f90
@@ -131,6 +131,7 @@ function worker_init(mdl) result(pst)
   use tree_formation_module, only: r_tree_formation,r_tree_clump
   use feedback_module, only: r_thermal_feedback, r_mechanical_feedback
   use sink_evolution_module, only: r_sink_evolution
+  use sink_merger_module, only: r_sink_merger
   use newdt_fine_module, only: r_newdt_part,r_broadcast_dt,r_max_B_and_Q,r_max_sigma
   use rho_fine_module, only: r_split_part,r_sort_part
 #ifdef GRAV
@@ -250,6 +251,7 @@ function worker_init(mdl) result(pst)
   call mdl_add_service(pst%s%mdl,MDL_THERMAL_FEEDBACK,       pst,C_FUNLOC(r_thermal_feedback),1,2,"thermal_feedback")
   call mdl_add_service(pst%s%mdl,MDL_MECHANICAL_FEEDBACK,    pst,C_FUNLOC(r_mechanical_feedback),1,2,"mechanical_feedback")
   call mdl_add_service(pst%s%mdl,MDL_SINK_EVOLUTION,         pst,C_FUNLOC(r_sink_evolution),1,2,"sink_evolution")
+  call mdl_add_service(pst%s%mdl,MDL_SINK_MERGER,            pst,C_FUNLOC(r_sink_merger),1,0,"sink_merger")
   call mdl_add_service(pst%s%mdl,MDL_NEWDT_PART,             pst,C_FUNLOC(r_newdt_part),0,0,"newdt_part")
   call mdl_add_service(pst%s%mdl,MDL_MAX_B_AND_Q,            pst,C_FUNLOC(r_max_B_and_Q),1,4,"max_B_and_Q")
   call mdl_add_service(pst%s%mdl,MDL_MAX_SIGMA,              pst,C_FUNLOC(r_max_sigma),1,2,"max_sigma")
