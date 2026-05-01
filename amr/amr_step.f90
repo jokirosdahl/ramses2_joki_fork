c31b979da4abd04a7ff599eb4e66ff2e64249fe1
James D. Sunseri <js7501@princeton.edu>
2026-05-01 03:03:18 -0400

    call the clump finder every 2 main steps

--- amr/amr_step.f90
+++ amr/amr_step.f90
@@ -87,7 +87,7 @@ recursive subroutine m_amr_step(pst,ilevel,icount,done)
   !--------------------------------
   ! Merging tree particle formation
   !--------------------------------
-  if(r%tree.and.ilevel==r%levelmin)then
+  if(r%tree.and.ilevel==r%levelmin.and.mod(g%nstep_coarse, 3)==0)then
      call m_timer(pst,'tree - formation','start')
      call m_tree_formation(pst)
   endif
